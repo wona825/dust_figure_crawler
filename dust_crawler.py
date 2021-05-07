94b3a4f6551075d93da95897a13a700b11c05bbd
jsrsogny <ryansally00@naver.com>
2021-05-07 23:04:55 +0900

    update by student2

--- dust_crawler.py
+++ dust_crawler.py
@@ -9,19 +9,37 @@ with urlopen('https://search.naver.com/search.naver?sm=tab_hty.top&where=nexearc
 
     dust = soup.find('div',{'class':'tb_scroll'})
     #print(dust)
+
     dust_region_information = dust.select('th')
-    print(dust_region_information)
+
 
     dust_region = []
     for i in dust_region_information:
-        i.text
+        i.string
         dust_region.append(i.text)
-    print(dust_region)
+
     dust_region.remove('관측지점')
     dust_region.remove('현재')
     dust_region.remove('오전예보')
     dust_region.remove('오후예보')
     print(dust_region)
 
-    dust_figure_information = dust.select('td')
-    print(dust_figure_information)
+    dust_figure = []
+    dust_figure_information = dust.find_all('span')
+    for i in dust_figure_information:
+        k = i.get_text()
+        dust_figure.append(k)
+    figure = []
+    for j in range(len(dust_figure)):
+        if j%3 == 0:
+            figure.append(dust_figure[j])
+    print(figure)
+
+
+
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+    plt.rcParams['font.family'] = 'Malgun Gothic'
+    plt.title('지역별 미세먼지 농도')
+    plt.bar(x, figure)
+    plt.xticks(x, dust_region)
+    plt.show()
